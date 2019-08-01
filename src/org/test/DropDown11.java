67f358615cd1d556291c17a6637cde66ff3540b8
ashwini91118 <ashwini9736@gmail.com>
2019-08-01 09:42:36 +0530

    add new class DropDown

---
+++ src/org/test/DropDown11.java
@@ -0,0 +1,10 @@
+package org.test;
+
+
+
+public class DropDown11 {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","D:\\Selenium\\softwares\\eclipse\\Project Practice\\SeleniumAshu\\diver\\chromedriver.exe");
+	
+	}
+}
